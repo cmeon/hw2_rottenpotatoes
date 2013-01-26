202bbcbeb4c3827dd6d9b220e468a7f03463c82c
cmeon <cmeo226@yahoo.com>
2013-01-26 11:45:34 +0800

    options are now changed to redirect their routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,35 +11,41 @@ class MoviesController < ApplicationController
     Movie.ratings.each do |rating|
       @all_ratings[rating] = true
     end
-      #ratings for checkbox
+    
+    #ratings for checkbox
     if params[:ratings]
       @all_ratings.each do |rating, check|
           @all_ratings[rating] = false unless params[:ratings].key?(rating)
       end
-      @checked_ratings = params[:ratings].keys
+      session[:all_ratings] = @all_ratings
       session[:ratings] = params[:ratings].keys
-    elsif session[:ratings]
+      redirect_to movies_path
+    end
+    
+    #checkboxes checkboxed
+    if session[:ratings]
       @checked_ratings = session[:ratings]
     else
       @checked_ratings = @all_ratings.keys
     end
     
+    #CSS highlighting changes
+    if session[:sorting_order] == "title"
+      @title_tab = "hilite"
+    elsif  session[:sorting_order] == "release_date"
+      @release_date_tab = "hilite"
+    end
+    
+    #movie rendering in specified order
     if params[:sort]
       session[:sorting_order] = params[:sort]
-      #CSS highlighting changes
-      if session[:sorting_order] == "title"
-        @title_tab = "hilite"
-      elsif  session[:sorting_order] == "release_date"
-        @release_date_tab = "hilite"
-      end
-      
-      #movie rendering in specified order
-      @movies = Movie.find(:all, :conditions => {:rating => @checked_ratings}, :order => "#{session[:sorting_order]}")
-    else
-      @movies = Movie.find(:all, :conditions => {:rating => @checked_ratings}, :order => "#{session[:sorting_order]}")
+      redirect_to movies_path
     end
+    
+    @all_ratings = session[:all_ratings]
+    @movies = Movie.find(:all, :conditions => {:rating => @checked_ratings}, :order => "#{session[:sorting_order]}")
   end
-
+  
   def new
     # default: render 'new' template
   end
